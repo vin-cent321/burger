01621f9102ec4a8ec8ef80b04a43472cc8fdbb35
Vincent Caracciolo <vincentncaracciolo@gmail.com>
2020-01-18 13:52:26 -0500

    more heroku fix

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,17 @@ var connection = mysql.createConnection({
   database: "burger"
 });
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'hacktheplanet',
+    database: 'todoagain_db'
+  });
+};
+
 // Make connection.
 connection.connect(function(err) {
   if (err) {
